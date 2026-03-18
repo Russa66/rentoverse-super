a189014d0ea7fbac64e201cbd8d86144a9789811
Russa Palit <russa.palit@gmail.com>
2026-03-18 20:01:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-184067128-73095",
+  "appId": "1:956580757155:web:75ee5c037f5de4818b8916",
+  "apiKey": "AIzaSyALTzhVC2LqEJzrQNlfGEyDYPo7FSP2ZVM",
+  "authDomain": "studio-184067128-73095.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "956580757155"
+};
